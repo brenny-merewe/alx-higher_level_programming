7aa77d203d227e7e86d9c27f20fd0acdd22aecc7
Brenny Merewe <bmerewe@gmail.com>
2023-03-07 23:56:06 -0800

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= int(ord('a') and ord(c) <= int(ord('z'):
+    if ord(c) >= int(ord('a')) and ord(c) <= int(ord('z')):
         return True
     else:
         return  false
